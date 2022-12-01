b4037d1288f3300b4c08cce8d631e919e7f58b56
SabbirAhmedAdor629 <sabbirgitrepository@gmail.com>
2022-12-01 09:22:11 +0600

    1 december

--- Postgres/main.go
+++ Postgres/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -11,8 +12,8 @@ const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
-	password = "sabbir123"
-	dbname   = "cmddb"
+	password = "postgres"
+	dbname   = "messaging_development"
 )
 
 func main() {
@@ -24,28 +25,49 @@ func main() {
 	// close database
 	defer db.Close()
 
-	//     // Delete
+
+	// 		// Query
+	// rows, err := db.Query(`SELECT "campaign_execution_log_id", "status" FROM "campaign_execution_log_chunks"`)
+	// CheckError(err)
+	
+	// defer rows.Close()
+	// for rows.Next() {
+	// 	var campaign_execution_log_id int
+	// 	var status string
+	
+	// 	err = rows.Scan(&campaign_execution_log_id, &status)
+	// 	CheckError(err)
+	
+	// 	fmt.Println(campaign_execution_log_id, status)
+	// }
+	// CheckError(err)
+
+	    // Delete
 	// deleteStmt := `delete from "students" where std_id=$1`
-	// _, e := db.Exec(deleteStmt, 6)
+	// _, e := db.Exec(deleteStmt, 4)
 	// CheckError(e)
 
-	// Update
-	// updateStmt := `update "students" set "std_name"=$1 where "std_id"=$2`
-	// _, e := db.Exec(updateStmt, "Mutakabbir Ahmed", 3)
+			// Update
+	// updateStmt := `UPDATE "campaign_execution_log_chunks" SET "campaign_id"=$1, "status"=$2 WHERE "id"=$3`
+	// _, e := db.Exec(updateStmt, 514, "will be updated", 6)
 	// CheckError(e)
 
+	
 	// Insertion
 	//  // insert
 	// insertStmt := `insert into "students"("std_id", "std_name", "std_program", "std_stream") values(4, 'Samin', 'School', 'Primary')`
 	// _, e := db.Exec(insertStmt)
 	// CheckError(e)
-	// dynamic
-	// insertDynStmt := `insert into "students"("std_id", "std_name", "std_program", "std_stream") values($1, $2, $3, $4)`
-	// _, e := db.Exec(insertDynStmt, 6, "Mutashim", "Engineering", "Mechanica")
+
+	// //dynamic
+	// insertDynStmt := `insert into "campaign_execution_log_chunks"("id",
+	// "campaign_execution_log_id", "campaign_id","utc_hour","execution_mins","processed_players",
+	// "eligible_players","status","status_updated_at","distinct_players_sent","push_messages_sent",
+	// "created_at","updated_at") values($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)`
+	// _, e := db.Exec(insertDynStmt, 4, nil, 4, 8, 9, nil, 16, "not-updated", time.Now(), 18, 19, time.Now(), time.Now())
 	// CheckError(e)
 
 	// Checking Connection
-	//     // check db
 	// err = db.Ping()
 	// CheckError(err)
 	// fmt.Println("Connected!")
